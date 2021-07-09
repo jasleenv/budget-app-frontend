0193511015021fb36e47b50700a58e1a7fa1df03
Jasleen Villamil <jasleenvillamil@gmail.com>
2021-07-09 14:53:07 -0400

    Saving again

--- src/Util/apiUrl.js
+++ src/Util/apiUrl.js
@@ -2,6 +2,6 @@ export const apiUrl = () => {
 
     return window.location.hostname === "localhost"
     ? "http://localhost:3003"
-    : ""
+    : "https://whispering-beyond-68503.herokuapp.com"
 //comments change it when I deploy herokuapp
 }
